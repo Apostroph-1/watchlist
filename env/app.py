e880079339d0accfc6bb07f791ab5a59ae1197af
gao <1951383615@qq.com>
2023-08-28 15:52:09 +0800

    Add databases support with Flask-SQLAlchemy

--- env/app.py
+++ env/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template,url_for
 from flask_sqlalchemy import SQLAlchemy #数据库
 import os
 import sys
+import click
 
 WIN = sys.platform.startswith('win')
 if WIN:  # 如果是 Windows 系统，使用三个斜线
@@ -27,6 +28,44 @@ class Movie(db.Model):#表名 Movie
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
 
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Shelton'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
 
 #
 # @app.route('/')
@@ -55,26 +94,14 @@ class Movie(db.Model):#表名 Movie
 #     return 'Test page'
 
 
-name = 'Shelton'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
-
 
 @app.route('/')
 def index():
+    user = User.query.first() #读取用户记录
+    movies = Movie.query.all() #读取所有的电影记录
     return render_template("index.html", name=name, movies=movies)
 
 
 
 if __name__ == '__main__':
-    app.run(port=5001,debug=True)
+    app.run(port=5001,debug=True) #port最后的序号 改为5001
